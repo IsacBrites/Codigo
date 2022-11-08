3663c99176bb4693eb3f8cfd6c053b6c155effd0
IsacBrites <103151852+IsacBrites@users.noreply.github.com>
2022-11-08 19:17:10 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-  Scanner ler = new Scanner(System.in);
-  int numero = 494943;
-  ContaBancaria  Silverio_Antonio = new ContaBancaria();
-  Silverio_Antonio.numero = numero;
-  Silverio_Antonio.nome = "Silvério Antônio";
-  System.out.println("\nO seu saldo é "+Silverio_Antonio.consultarSaldo());
-  System.out.println("\nO seu limite é "+Silverio_Antonio.consultarLimite());
+    Scanner ler = new Scanner(System.in);
+    int valor1, valor2;
+
+    System.out.println("Insira um numero inteiro: ");
+    valor1 = ler.nextInt();
+
+    System.out.println("Insira outro numero inteiro: ");
+    valor2 = ler.nextInt();
+    primeiraClass nome = new primeiraClass(valor1, valor2);
+
+    int soma = nome.Soma();
+    System.out.println("Soma: \n" + soma);
+
+    int subt = nome.Subt();
+    System.out.println("Subtração: \n" + subt);
+
+    int mult = nome.Mult();
+    System.out.println("Multiplicação: \n" + mult);
+
+    int div = nome.Divs();
+    System.out.println("Divisão: \n" + div);
   }
 }
---
+++ primeiraClass.java
@@ -0,0 +1,33 @@
+public class primeiraClass {
+   boolean ClasseLegal = true;
+
+   private int arg1 = 0;
+   private int arg2 = 0;
+
+   public primeiraClass(int num1, int num2) {
+      arg1 = num1;
+      arg2 = num2;
+
+   }
+
+   public int CodigoDaClasse() {
+      return 1234;
+   }
+
+   public int Soma() {
+      return arg1 + arg2;
+   }
+
+   public int Mult() {
+      return arg1 * arg2;
+   }
+
+   public int Divs() {
+      return arg1 / arg2;
+   }
+
+   public int Subt() {
+      return arg1 - arg2;
+   }
+
+}
