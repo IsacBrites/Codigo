1a8897bd45024bb5ffa344d10fe8230112b96074
IsacBrites <103151852+IsacBrites@users.noreply.github.com>
2022-11-17 21:15:04 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,38 +1,61 @@
+import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Arrays;
-import java.io.*;
-
-
+import java.util.Collections;
 class Main {
   public static void main(String[] args) {
-     Scanner ler = new Scanner(System.in);
-    int maior =0;
-     int[][] matriz = new int[4][5];
-  String nomes[][] ={ 
-{"João", "Pedro", "Maria", "Clara", "Lucas"},
-{"Pedro", "Maria"}, 
-{"Pedro", "Maria", "Clara"},
-{"João" , "Pedro" , "Lucas"}
-};  
-  System.out.println(Arrays.deepToString(nomes[0]));
-  System.out.println(Arrays.deepToString(nomes[1]));
-  System.out.println(Arrays.deepToString(nomes[2]));
-  System.out.println(Arrays.deepToString(nomes[3]));
-  for(int linha=0 ; linha < matriz.length ; linha++){
-  for(int coluna = 0; coluna < matriz[linha].length ; coluna++){
-  System.out.printf("\nQuem possui amizade com quem? [%d][%d]: ",linha+1,coluna+1);
-  matriz[linha][coluna]=ler.nextInt();
-       if(matriz[linha][coluna] > maior) { 
-      maior = matriz[linha][coluna];
-    }
+  Scanner ler = new Scanner(System.in);
+  LinkedList<String> esporte = new LinkedList<>();
+  esporte.add("Futebol");
+  esporte.add("Vôlei");
+  esporte.add("Natação");
+  esporte.add("Polo aquático");
+  esporte.add("Hipismo");
+  esporte.add("Basquete");
+  esporte.add("Handball");
+  esporte.add("Corrida");
+  esporte.add("Golfe");
+  esporte.add("Salto com vara");
   
-  }}
-  System.out.println("\nAs amizades são: \n");
-  for(int linha=0 ; linha < matriz.length ; linha++){
-  for(int coluna = 0; coluna < matriz[linha].length ; coluna++){
-  System.out.print(matriz[linha][coluna]); }
-  System.out.println();
-}
+    
+  System.out.printf("Percorrendo a lista:\n");
+  System.out.printf("\n");
+  int i,num,elemento1,elemento2;
+  for (i=0; i<esporte.size(); i++) {
+  System.out.printf("Posição %d %s\n", i, esporte.get(i));
+    }
+  System.out.printf("\n");
 
-}
-}
+
+  System.out.printf("Elemento adicionado:\n\n");
+    esporte.add(0,"Jiu-jitsu");
+  for (i=0; i<esporte.size(); i++) {
+    System.out.printf("Posição %d %s\n", i, esporte.get(i));
+    }
+  System.out.printf("\n");
+
+
+  System.out.printf("Elemento que deseja acessar? ");
+  num = ler.nextInt();
+  System.out.printf("\n");
+  System.out.printf("Elemento acessado é " + esporte.get(num));
+  
+    
+  System.out.printf("\n\nLista organizada: \n");
+  Collections.sort(esporte);
+  for (String org : esporte) {
+    System.out.println(org);
+    }
+
+
+  System.out.printf("\n\nLista randomizada: \n");
+  Collections.shuffle(esporte);
+  for (String random : esporte) {
+    System.out.println(random);
+    }
+  System.out.printf("\n\nLista ao contrário: \n");
+  Collections.sort(esporte, Collections.reverseOrder());
+  for (String reverse : esporte) {
+    System.out.println(reverse);
+    }
+}}
